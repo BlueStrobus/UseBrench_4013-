b3600be39f782b11a490d39d68e012adfdb4071d
strobus <116132298+BlueStrobus@users.noreply.github.com>
2023-04-14 09:35:59 +0900

    s

---
+++ JS04/Js0413/main/Copy.js
@@ -0,0 +1,13 @@
+let num =10;
+
+
+//깊은 복사
+let copyNum =num;
+// num이랑 copynum이 다른 곳에 저장 -> 10이 2개 저장
+
+num=220;
+console.log(copyNum,num);
+
+const person = { name:"gill"};
+person ={name:"hong"};
+console.log(person);
