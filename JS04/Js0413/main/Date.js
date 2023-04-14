5c0b118a141afbadc2ea7482b9b3392c755cabb8
strobus <116132298+BlueStrobus@users.noreply.github.com>
2023-04-14 10:53:51 +0900

    차이 찾기

--- JS04/Js0413/main/Date.js
+++ JS04/Js0413/main/Date.js
@@ -1,4 +1,13 @@
-const date = new Date();
-date.setFullYear(2222)
-const dateFormet = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
-console.log(date);
+const date1 = new Date("2022-12-23");
+const date2 = new Date("2022-12-25");
+const dateDiff = date2.getTime() - date1.getTime();
+const includes =dateDiff / (24*60*60*1000);
+console.log(`두 날짜의 차이는 ${includes}일 입니다.`)
+
+
+
+
+
+
+// const dateFormet = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
+// console.log(date);
