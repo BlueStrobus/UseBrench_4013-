afaea8b1dc9166661bd35b12f8b5cbac9f9b309f
strobus <116132298+BlueStrobus@users.noreply.github.com>
2023-04-14 10:21:43 +0900

    n

--- JS04/Js0413/main/String.js
+++ JS04/Js0413/main/String.js
@@ -2,3 +2,10 @@ const pw = "124";
 if(pw.length <4){
     console.log("비밀번호를 최소 4글자로 적어주세요")
 }
+
+
+const emall ="test!naver.com";
+if(emall.includes("@")){
+    console.log("true")
+}
+else{console.log("try again")}
