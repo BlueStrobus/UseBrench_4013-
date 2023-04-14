9dd2461dabeec836f90314af5fb255bbbc98b370
strobus <116132298+BlueStrobus@users.noreply.github.com>
2023-04-14 09:39:44 +0900

    ㄴ

--- JS04/Js0413/main/Copy.js
+++ JS04/Js0413/main/Copy.js
@@ -15,6 +15,9 @@ console.log(copyNum,num);
 // console.log(person);
 //const는 안되고 let는 됨
 
-const person = { name:"gill"};  //주소저장
+const person = { name:"gill"};  
+
+// person에는 주소저장
+//{ name:"gill"}는 다른 곳에 저장
 person.name="hong";
 console.log(person);
