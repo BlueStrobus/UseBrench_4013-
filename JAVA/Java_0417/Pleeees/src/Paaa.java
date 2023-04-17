e390f228b18336ae354627855930f4e1491de31c
strobus <116132298+BlueStrobus@users.noreply.github.com>
2023-04-17 14:31:07 +0900

    h

--- JAVA/Java_0417/Pleeees/src/Paaa.java
+++ JAVA/Java_0417/Pleeees/src/Paaa.java
@@ -3,7 +3,24 @@
 
 
 public class Paaa {
-    System.out.println("쫌!!! 되라고!!!");
-
     
+    public static void main(String[] args) {
+        Reso reso = new Reso();
+
+        try (reso) {
+            reso.show();
+        } catch (Exception e) {
+            System.out.println("예외 처리");
+        }
+    }
+}
+
+class Reso implements AutoCloseable {
+    void show() {
+        System.out.println("자원 사용");
+    }
+
+    public void close() throws Exception {
+        System.out.println("자원 닫기");
+    }
 }
